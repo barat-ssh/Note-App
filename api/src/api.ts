4707c181ce7a58da7871342c4ffa52bd05d53953
Barath Elangovan <barath_elangovan@outlook.com>
2023-12-28 20:38:50 +0530

    initial commit

---
+++ api/src/api.ts
@@ -0,0 +1,11 @@
+import express from "express";
+const app = express();
+const PORT = 3001;
+
+// connecting to db
+
+// importing routes
+
+app.listen(PORT, () => {
+  console.log("server hitting...");
+});
